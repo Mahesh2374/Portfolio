92938eed2c8b462fea093fee10a2d1d2bd3183d0
Mahesh2374 <84325926+Mahesh2374@users.noreply.github.com>
2022-07-11 17:44:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,14 +25,14 @@ $(document).ready(function () {
 
   //typing animation script
   var typed = new Typed(".typing", {
-    strings: ["Full-Stack Web Devolper", "Designer"],
+    strings: ["Full-Stack Web Devolper", "Full-Stack Web Devolper"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
   var typed = new Typed(".typing-2", {
-    strings: ["Full-Stack Web Devolper", "Designer"],
+    strings: ["Full-Stack Web Devolper", "Full-Stack Web Devolper"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
